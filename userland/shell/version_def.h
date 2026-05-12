1488bc5bfa56273c19e3ef53cc9f5c940a96b0a1
BPForbes <62630945+BPForbes@users.noreply.github.com>
2026-05-12 13:25:13 +0000

    chore(version): sync version/locked from entries after merge

--- userland/shell/version_def.h
+++ userland/shell/version_def.h
@@ -15,7 +15,7 @@
  */
 #define VERSION_MAJOR    3
 #define VERSION_STANDARD 2
-#define VERSION_PATCH    1
+#define VERSION_PATCH    2
 
 #define VERSION_STR_(x) #x
 #define VERSION_STR(x) VERSION_STR_(x)
